c3d43b9cab4c4347691d67006ff050f5a3dc38f7
Rohit Sachdeva <growth@drinnovations.us>
2014-09-08 15:35:48 -0500

    equalities - with logging integrated

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject clojure-koans "0.5.0-SNAPSHOT"
   :description "The Clojure koans."
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [log4j/log4j "1.2.17"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]
                  [koan-engine "0.2.3"]]
   :dev-dependencies [[lein-koan "0.1.3"]]
   :profiles {:dev {:dependencies [[lein-koan "0.1.3"]]}}
