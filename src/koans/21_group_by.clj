d0e2bed90c07e59f6d91bddf8fe34e9f31014db5
Rohit Sachdeva <growth@drinnovations.us>
2014-09-26 12:26:42 -0500

    Group By, All koans solved currently Sept 2014 - You have achieved clojure enlightenment. Namaste.

--- src/koans/21_group_by.clj
+++ src/koans/21_group_by.clj
@@ -1,23 +1,28 @@
 (ns koans.21-group-by
-  (:require [koan-engine.core :refer :all]))
+  (:require [koan-engine.core :refer :all]
+            [clojure.tools.logging :refer [trace debug spy info]]))
 
 (defn get-odds-and-evens [coll]
-  (let [{odds true evens false} (group-by __ coll)]
+  ; using destructuring odda and even could be any thing from boolean function having true and false
+  ; same result for function is grouped together
+  (let [{odds true evens false} (group-by odd? coll)]
     [odds evens]))
 
 (meditations
 
   "To categorize a collection by some function, use group-by."
-  (= __ (group-by count ["hello" "world" "foo" "bar"]))
+  (= {3 ["foo" "bar"], 5 ["hello" "world"]} (group-by count ["hello" "world" "foo" "bar"]))
 
 
+  ; using juxt!
   "You can simulate filter + remove in one pass"
   (= (get-odds-and-evens [1 2 3 4 5])
      ((juxt filter remove) odd? [1 2 3 4 5])
      [[1 3 5] [2 4]])
 
   "You can also group by a primary key"
-  (= __
+  (= {1 [{:id 1 :name "Bob"}, {:id 1 :last-name "Smith"}]
+      2 [{:id 2 :name "Mike"}]}
      (group-by :id [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"} ]))
@@ -25,13 +30,15 @@
   "But be careful when you group by non-required key"
   (= {"Bob" [{:name "Bob" :id 1}]
       "Mike" [{:name "Mike" :id 2}]
-      __ [{:last-name "Smith" :id 1}]}
+      nil [{:last-name "Smith" :id 1}]}
    (group-by :name [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"}]))
 
   "The true power of group-by comes with custom functions"
-  (= __
+  ; same result for function is grouped together
+  (= {:naughty-list [{:name "Jimmy" :bad true}, {:name "Joe" :bad true}]
+      :nice-list [ {:name "Jack" :bad false}]}
      (group-by #(if (:bad %) :naughty-list :nice-list)
                [{:name "Jimmy" :bad true}
                 {:name "Jack" :bad false}
